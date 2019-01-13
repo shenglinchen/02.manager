ce5d1c870f3e8a1a28ec7c17f0de31d726d1e98a
Nikola chen <linshengchen96@gmail.com>
2019-01-13 11:58:08 +0800

    登录页的布局

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <div class="login">
+      <h2>用户登录</h2>
+      <el-form label-position="top" label-width="80px" :model="formLabelAlign">
+        <el-form-item label="name">
+          <el-input v-model="formLabelAlign.name"></el-input>
+        </el-form-item>
+        <el-form-item label="password">
+          <el-input v-model="formLabelAlign.password"></el-input>
+        </el-form-item>
+        
+      </el-form>
+      <el-button type="primary" class="btn-login">登录</el-button>
+    </div>
+    
+    
   </div>
 </template>
 
@@ -8,18 +22,42 @@
 
 export default {
   name: 'app',
-  components: {
+  data() {
+    return {
+      formLabelAlign: {
+        name: '',
+        password: '',
+      }
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  html, body {
+    height: 100%;
+
+  }
+
+  body {
+    background-color: rgb(50, 65, 82);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    .login {
+      box-sizing: border-box;
+      width: 600px;
+      background-color: #fff;
+      border-radius: 5px;
+      padding: 40px;
+      .btn-login {
+        width: 100%;
+      }
+    }
+  }
+
+  
+
+  
 </style>
