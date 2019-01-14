bc251b8618a7dee7c1e34a8ef027bb026deef3b8
Nikola chen <linshengchen96@gmail.com>
2019-01-14 10:27:18 +0800

    完成导航守卫，让每次进入后台都要验证

--- src/components/index.vue
+++ src/components/index.vue
@@ -7,12 +7,13 @@
 <script>
 	export default {
 		name: "index",
-		beforeCreate() {
-			if (!window.sessionStorage.getItem("token")) {
-                this.$message.error("请先登录");
-				this.$router.push("login");
-			}
-		}
+		// 设置导航守卫后，不需要了
+		// beforeCreate() {
+		// 	if (!window.sessionStorage.getItem("token")) {
+  //               this.$message.error("请先登录");
+		// 		this.$router.push("login");
+		// 	}
+		// }
 	}
 </script>
 
--- src/lib/vue-router.js
+++ src/lib/vue-router.js
@@ -27,4 +27,27 @@ const router = new VueRouter ({
 	routes
 })
 
+
+// 设置导航守卫：在路由跳转时执行的回调函数
+
+router.beforeEach((to, from, next) => {
+	// to去哪里
+	// from从哪里来
+	// next执行下一个函数
+	// console.log(to);
+	// console.log(from);
+	if (to.path == "/login") {
+		next();
+	}else {
+		if (!window.sessionStorage.getItem("token")) {
+			// console.log(to)
+		    Vue.prototype.$message.error("请先登录");
+			// Vue.prototype.$router.push("login");为什么不行
+			next("/login")
+
+		}else {
+			next();
+		}
+	}
+})
 export default router;
