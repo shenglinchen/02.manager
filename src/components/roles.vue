3972faa33ea39690e6c5c1b37b9d4398201c6f3f
Nikola chen <linshengchen96@gmail.com>
2019-01-29 14:53:53 +0800

    完成了所有不重复的地方，即完成该项目

--- src/components/roles.vue
+++ src/components/roles.vue
@@ -6,17 +6,36 @@
 		    </el-col>
 		</el-row>
 
-		<el-button type="primary" plain>
+		<el-button type="primary" plain @click="addVisible = true">
 			添加角色
 		</el-button>
+		<!-- 添加角色 -->
 
+		<el-dialog title="添加角色" :visible.sync="addVisible">
+		  <el-form :model="addForm" :rules="addValues" :ref="addForm">
+		    <el-form-item label="角色名称" label-width="100px" prop="roleName">
+		      <el-input v-model="addForm.roleName"  autocomplete="off"></el-input>
+		    </el-form-item>		  		    
+		    <el-form-item label="角色描述" label-width="100px" prop="roleDesc">
+		      <el-input v-model="addForm.roleDesc"  autocomplete="off"></el-input>
+		    </el-form-item>
+
+		  </el-form>
+		  <div slot="footer" class="dialog-footer">
+		    <el-button @click="addVisible = false">取 消</el-button>
+		    <el-button type="primary" @click="submitAdd(addForm)">确 定</el-button>
+		  </div>
+		</el-dialog>
+		
+
+		<!-- 角色管理 -->
 		<el-row>
 			<el-table border :data="rolesData" style="width: 100%">
 			  	<el-table-column type="expand">
 			  		<template slot-scope="props">
 			  			<el-row v-for="level1 in props.row.children" :key="level1.id">
 			  				<el-col :span="4">
-			  					<el-tag closable >
+			  					<el-tag closable @close="removeRight(props.row, level1.id)">
 			  						{{level1.authName}}
 			  						
 			  					</el-tag>
@@ -24,12 +43,12 @@
 			  				<el-col :span="20">
 			  					<el-row v-for="level2 in level1.children" :key="level2.id">
 			  						<el-col :span="8">
-			  							<el-tag closable type="success">
+			  							<el-tag closable type="success" @close="removeRight(props.row, level2.id)">
 			  								{{level2.authName}}
 			  							</el-tag>
 			  						</el-col>
 			  						<el-col :span="16">
-			  							<el-tag v-for="level3 in level2.children" :key="level3.id" type="warning" closable>
+			  							<el-tag v-for="level3 in level2.children" :key="level3.id" type="warning" closable @close="removeRight(props.row, level3.id)">
 			  								{{level3.authName}}
 			  							</el-tag>
 			  						</el-col>
@@ -51,14 +70,57 @@
 			  	</el-table-column>
 			  	<el-table-column label="操作">
 					<template slot-scope="scope">
-						<el-button type="primary" icon="el-icon-edit" plain size="mini"></el-button>
-						<el-button type="success" icon="el-icon-delete" plain size="mini"></el-button>
-						<el-button type="danger" icon="el-icon-check" plain size="mini"></el-button>
+						<el-button type="primary" icon="el-icon-edit" plain size="mini" @click="editRole(scope.row)"></el-button>
+						<el-button type="success" icon="el-icon-delete" plain size="mini" @click="delRole(scope.row)"></el-button>
+						<el-button type="danger" icon="el-icon-check" plain size="mini" @click="distriRig(scope.row)"></el-button>
 					</template>
 	
 			  	</el-table-column>
 			</el-table>
 		</el-row>
+		
+		<!-- 编辑角色弹出框 -->
+		<el-dialog
+		  title="编辑角色"
+		  :visible.sync="editVisible"
+		  width="30%">
+			
+			<el-form :model="editRoleForm" :rules="rules" ref="editRoleForm" label-width="100px">
+			  	<el-form-item label="角色名称" prop="roleName">
+			  	  <el-input v-model="editRoleForm.roleName"></el-input>
+			  	</el-form-item>
+			  	<el-form-item label="角色描述" prop="roleDesc">
+			  	  <el-input v-model="editRoleForm.roleDesc"></el-input>
+			  	</el-form-item>
+			 </el-form>
+
+		  	<span slot="footer" class="dialog-footer">
+		  	  <el-button @click="editVisible = false">取 消</el-button>
+		  	  <el-button type="primary" @click="submitEdit()">确 定</el-button>
+		  	</span>
+		</el-dialog>
+
+		<!-- 分配权限的树形控件 -->
+		<el-dialog
+		  title="分配权限"
+		  :visible.sync="distriRigVisible"
+		  width="30%">
+
+		  <el-tree
+		  	ref="tree"
+		    :data="distriRights"
+		    show-checkbox
+		    node-key="id"
+		    :default-expanded-keys="checkedRights"
+		    :default-checked-keys="checkedRights"
+		    :props="defaultProps">
+		  </el-tree>
+
+		  <span slot="footer" class="dialog-footer">
+		    <el-button @click="distriRigVisible = false">取 消</el-button>
+		    <el-button type="primary" @click="submitDistri()">确 定</el-button>
+		  </span>
+		</el-dialog>
 
 
 	</div>
@@ -80,15 +142,53 @@
 					pagenum: 1,
 					pagesize: 10
 				},
-				total: 0
+				total: 0,
+				addVisible: false,
+				addForm: {
+					username: "",
+					desc: ""
+				},
+				addValues: {
+				  roleName: [
+				    { required: true, message: '请输入角色名称', trigger: 'change' },
+				    { min: 3, max: 32, message: '长度在 3 到 32 个字符', trigger: 'change' }
+				  ],
+				  roleDesc: [
+				    { required: true, message: '请输入描述', trigger: 'change' },
+				    { min: 3, max: 32, message: '长度在 3 到 32 个字符', trigger: 'change' }
+				  ],
+				},
+				editVisible: false,
+				editRoleForm: {
+					roleName: "",
+					roleDesc: ""
+				},
+				rules: {
+				  roleName: [
+				    { required: true, message: '请输入角色名称', trigger: 'change' },
+				  ],
+				  roleDesc: [
+				    { required: true, message: '请输入角色描述', trigger: 'change' },
+				  ]
+				},
+				editId: 0,
+				// 分配权限
+				defaultProps: {
+					label: "authName",
+					children: "children"
+				},
+				distriRights: [],
+				distriRigVisible: false,
+				checkedRights: [],
+				distriId: 0
+
 			}
 		},
 		// 使用async来让从上到下执行
 		async created(){
 
-			let res = await this.$axios.get("roles");
-
-			this.rolesData = res.data.data;
+			this.getRoles();
+			// console.log(this.rolesData)
 			// console.log(res.data.data);
 
 			// this.$axios.get("users", {
@@ -101,7 +201,106 @@
 			// .catch(err => {
 			// 	console.log(err);
 			// })
-		}
+		},
+		methods: {
+			async getRoles() {
+				let res = await this.$axios.get("roles");
+
+				this.rolesData = res.data.data;
+			},
+			async removeRight(user, rightId) {
+				// console.log(this.rolesData)
+				// console.log(user.id);
+				// console.log(rightId);
+				let res = await this.$axios.delete(`roles/${user.id}/rights/${rightId}`);
+				// console.log(res);
+				user.children = res.data.data;
+				// console.log(user)
+
+
+			},
+			submitAdd (formName) {
+        		this.$refs[formName].validate(async (valid) => {
+        		  if (valid) {
+        		  	console.log(formName)
+        		  	let res = await this.$axios.post("roles", formName);
+        		  	console.log(res);
+        		  	this.getRoles();
+        		  	this.addVisible = false;
+        		  	this.addForm = {};
+        		  } else {
+        		    console.log('error submit!!');
+        		    return false;
+        		  }
+        		});
+      		},
+
+      		editRole(data) {
+      			this.editVisible = true;
+      			console.log(data)
+      			this.editRoleForm.roleName = data.roleName;
+      			this.editRoleForm.roleDesc = data.roleDesc;
+
+      			this.editId = data.id;
+      		},
+      		async submitEdit() {
+      			this.editVisible = false;
+      			await this.$axios.put(`roles/${this.editId}`, this.editRoleForm);
+
+      			this.getRoles();
+      		},
+      		// 删除角色
+      		async delRole(data) {
+      		// 弹出
+      		 this.$confirm('是否删除该角色', '警告', {
+      		   confirmButtonText: '确定',
+      		   cancelButtonText: '取消',
+      		   type: 'warning'
+      		 }).then(async () => {
+      			await this.$axios.delete(`/roles/${data.id}`);
+      		   	await this.getRoles();
+      		 }).catch(() => {
+      		   this.$message({
+      		     type: 'info',
+      		     message: '已取消删除'
+      		   });          
+      		  });
+      		},
+      		// 分配权限
+      		async distriRig(data) {
+      			this.checkedRights = [];
+      			this.distriId = data.id;
+      			this.distriRigVisible = true;
+      			let backData = await this.$axios.get("/rights/tree");
+      			this.distriRights = backData.data.data;
+      			// 递归获取已经被勾选的权限
+      			let findChildren = (father) => {
+      				// 如果该数据有子元素，那么遍历子元素，继续虚招子元素中的被选中的
+      				if (father.children != undefined) {
+      					father.children.forEach(v => {
+      						findChildren(v);
+      					})
+      				}else {
+      					this.checkedRights.push(father.id);
+      				}
+      			}
+
+      			// 在data中存有该角色的权限
+      			findChildren(data);
+      		},
+      		// 提交分配的权限
+      		async submitDistri() {
+      			let checkedKey = this.$refs.tree.getCheckedKeys().join(",");
+      			console.log(this.distriId)
+      			let backdata = await this.$axios.post(`roles/${this.distriId}/rights`, {
+      				rids: checkedKey
+      			});
+      			console.log(backdata)
+      			this.getRoles();
+      			this.distriRigVisible = false;
+      		}
+      	}
+
 	}
 </script>
 
--- src/components/users.vue
+++ src/components/users.vue
@@ -7,14 +7,36 @@
 		</el-row>
 		<el-row>
 		  	<el-col :span="8">		  		
-		  		<el-input placeholder="请输入内容">
+		  		<el-input placeholder="请输入内容" v-model="dataQuery.query">
 	
-		  		  <el-button slot="append" icon="el-icon-search"></el-button>
+		  		  <el-button slot="append" icon="el-icon-search" @click="search" ></el-button>
 		  		</el-input>
 		  	</el-col>
 
 		  	<el-col :span="3">
-		  		<el-button type="success" plain>添加用户</el-button>
+		  		<el-button type="success" plain @click="dialogFormVisible = true" >添加用户</el-button>
+		  		<el-dialog title="用户修改" :visible.sync="dialogFormVisible">
+		  		  <el-form :model="addForm" :rules="rules">
+		  		    <el-form-item label="用户名" label-width="100px" prop="username">
+		  		      <el-input v-model="addForm.username"  autocomplete="off"></el-input>
+		  		    </el-form-item>		  		    
+		  		    <el-form-item label="密码" label-width="100px" prop="password">
+		  		      <el-input v-model="addForm.password"  autocomplete="off"></el-input>
+		  		    </el-form-item>
+		  		    <el-form-item label="邮箱" label-width="100px" prop="email">
+		  		      <el-input v-model="addForm.email"  autocomplete="off"></el-input>
+		  		    </el-form-item>
+		  		    <el-form-item label="电话" label-width="100px" prop="mobile">
+		  		      <el-input v-model="addForm.mobile"  autocomplete="off"></el-input>
+		  		    </el-form-item>
+
+
+		  		  </el-form>
+		  		  <div slot="footer" class="dialog-footer">
+		  		    <el-button @click="dialogFormVisible = false">取 消</el-button>
+		  		    <el-button type="primary" @click="submit">确 定</el-button>
+		  		  </div>
+		  		</el-dialog>
 		  	</el-col>
 		</el-row>
 		<el-row>
@@ -44,21 +66,67 @@
 			    width="250">
 			  </el-table-column>
 			  <el-table-column
-			    label="操作">
+			    label="用户状态">
 				<template slot-scope="scope">
 					<el-switch
 					  v-model="scope.row.mg_state"
 					  active-color="#13ce66"
-					  inactive-color="#ff4949">
+					  inactive-color="#ff4949" @change="updateStatus(scope.row.id, scope.row.mg_state)">
 					</el-switch>
 				</template>
 			  </el-table-column>
 			  <el-table-column
 			    label="操作">
-				<template slot-scope="scope">
-					<el-button type="primary" icon="el-icon-edit" plain size="mini"></el-button>
-					<el-button type="success" icon="el-icon-delete" plain size="mini"></el-button>
-					<el-button type="danger" icon="el-icon-check" plain size="mini"></el-button>
+				<template slot-scope="caozuoScope">
+					<el-button type="primary" icon="el-icon-edit" plain size="mini" @click="edit(caozuoScope.row)" ></el-button>
+					<el-button type="success" icon="el-icon-delete" plain size="mini" @click="deleteOne(caozuoScope.row.id)"></el-button>
+					<el-button type="danger" icon="el-icon-check" plain size="mini" @click="attributeRole(caozuoScope.row)"></el-button>
+
+					<!-- 编辑用户 -->
+					<el-dialog title="编辑用户" :visible.sync="editVisible">
+					  <el-form :model="editForm" :rules="editValues">
+					    <el-form-item label="用户名" label-width="100px" prop="username">
+					      <el-input v-model="editForm.username"  autocomplete="off" disabled></el-input>
+					    </el-form-item>		  		    
+					    <el-form-item label="邮箱" label-width="100px" prop="email">
+					      <el-input v-model="editForm.email"  autocomplete="off"></el-input>
+					    </el-form-item>
+					    <el-form-item label="电话" label-width="100px" prop="mobile">
+					      <el-input v-model="editForm.mobile"  autocomplete="off"></el-input>
+					    </el-form-item>
+
+
+					  </el-form>
+					  <div slot="footer" class="dialog-footer">
+					    <el-button @click="dialogFormVisible = false">取 消</el-button>
+					    <el-button type="primary" @click="submitEdit">确 定</el-button>
+					  </div>
+					</el-dialog>
+					
+					<!-- 编辑角色 -->
+					<el-dialog title="分配角色" :visible.sync="roleVisible">
+					  <el-form>
+					    <el-form-item label="当前用户" label-width="100px" prop="username">
+							{{currentUser.username}}
+					    </el-form-item>		  		    
+					    <el-form-item label="请选择角色" label-width="100px" prop="email">
+					    	<el-select v-model="currentUser.role_name" placeholder="请选择">
+					    	  <el-option
+					    	    v-for="item in allRoleData"
+					    	    :key="item.id"
+					    	    :label="item.roleName"
+					    	    :value="item.id">
+					    	  </el-option>
+					    	</el-select>
+					    </el-form-item>
+
+					  </el-form>
+					  <div slot="footer" class="dialog-footer">
+					    <el-button @click="roleVisible = false">取 消</el-button>
+					    <el-button type="primary" @click="submitRole">确 定</el-button>
+					  </div>
+					</el-dialog>
+
 				</template>
 
 			  </el-table-column>
@@ -94,22 +162,114 @@
 					pagenum: 1,
 					pagesize: 10
 				},
-				total: 0
+				total: 0,
+				dialogFormVisible: false,
+				addForm: {
+					username: "",
+					password: "",
+					email: "",
+					mobile: "",
+				},
+				rules: {
+				  username: [
+				    { required: true, message: '请输入用户名称', trigger: 'change' },
+				    { min: 3, max: 32, message: '长度在 3 到 32 个字符', trigger: 'change' }
+				  ],
+				  password: [
+				    { required: true, message: '请输入密码', trigger: 'change' },
+				    { min: 3, max: 32, message: '长度在 3 到 32 个字符', trigger: 'change' }
+				  ],
+				},
+				editVisible: false,
+				editForm: {
+					username: "",
+					email: "",
+					mobile: "",
+				},
+				editValues: {
+				  username: [
+				    { required: true, message: '请输入用户名称', trigger: 'change' },
+				    { min: 3, max: 32, message: '长度在 3 到 32 个字符', trigger: 'change' }
+				  ],
+				},
+				editId: 0,
+				// 分配角色
+				roleVisible: false,
+				currentUser: {},
+				selectValue: 0,
+				allRoleData: {}
+
+
 			}
 		},
 		created(){
-			this.$axios.get("users", {
-				params: this.dataQuery
-			}).then(res => {
+			this.getData();
+		},
+		methods: {
+			search() {
+				this.getData();
+			},
+			getData() {
+				console.log(this.dataQuery.query)
+				this.$axios.get("users", {
+					params: this.dataQuery
+				}).then(res => {
+					console.log(res);
+					this.tableData = res.data.data.users;
+					this.total = res.data.data.total;
+				})
+				.catch(err => {
+					console.log(err);
+				})
+			},
+			async submit() {
+				this.dialogFormVisible = false;
+				let res = await this.$axios.post("users", this.addForm);
+				this.getData();
+			},
+			async updateStatus(id, status) {
+				let res = await this.$axios.put(`users/${id}/state/${status}`);
+				console.log(res)
+			},
+			async deleteOne(id) {
+				let res = await this.$axios.delete(`users/${id}`);
 				console.log(res);
-				this.tableData = res.data.data.users;
-				this.total = res.data.data.total;
-			})
-			.catch(err => {
-				console.log(err);
-			})
+				this.getData();
+			},
+			async submitEdit() {
+				this.editVisible = false;
+				let res = await this.$axios.put(`users/${this.editForm.id}`, {
+					id: this.editForm.id,
+					email: this.editForm.email,
+					mobile: this.editForm.mobile
+				})
+			},
+			edit(obj) {
+				this.editVisible = true; 
+				this.editForm = obj;
+
+			},
+			// 分配用户
+			async attributeRole(currentUser) {
+				this.roleVisible = true;
+				this.currentUser = currentUser;
+				let res = await this.$axios.get("roles");
+				console.log(res);
+				console.log(currentUser);
+				this.allRoleData = res.data.data;
+				this.selectValue = currentUser.id;
+			},
+			async submitRole() {
+				this.roleVisible = false;
+				let res = await this.$axios.put(`users/${this.currentUser.id}/role`, {
+					rid: this.currentUser.role_name
+				});
+				console.log(res)
+			}
 		}
 	}
+
+
 </script>
 
 <style lang="scss">
